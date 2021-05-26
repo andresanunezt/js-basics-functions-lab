dd685619a8ce8715593cc3e0fee211568719d5df
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-25 20:16:51 -0400

    commit tree 8b931de63db2dfb47b87ea8cddcc9f61c5889dc2

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {  
+    if (blockNumber > 42) {
+        return blockNumber - 42;
+    }
+    else {
+        return 42 - blockNumber;
+    }
+}
+
+function distanceFromHqInFeet (blockNumber) {
+    let blocks = distanceFromHqInBlocks(blockNumber);
+    return blocks * 264;
+  }
+
+  function distanceTravelledInFeet (block1, block2) {
+    let blocks = Math.abs(block1 - block2);
+    return blocks * 264; 
+  }
+
+  function calculatesFarePrice (start, end) {
+    let distance = distanceTravelledInFeet(start, end);
+    let fare = 0;
+    if(distance <= 400){
+        return fare;
+    }
+    else if(distance > 400 && distance <= 2000){
+        fare = (distance - 400) * 0.02;
+        return fare;
+    }
+    else if(distance > 2000 && distance <= 2500){
+        fare = 25;
+        return fare;
+    }
+    else{
+        return "cannot travel that far";
+    }
+  }
